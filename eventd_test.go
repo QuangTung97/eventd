474d339bc7dedb93130bd35cd652153927e3ca97
Tùng Tạ Quang <tung.tq@teko.vn>
2021-04-13 14:16:31 +0700

    Add Processor Run Error Test Case

--- eventd_test.go
+++ eventd_test.go
@@ -147,3 +147,73 @@ func TestRunner__GetLastEvents_Error_Context_Cancelled(t *testing.T) {
 	assert.Equal(t, ctx, calls[0].Ctx)
 	assert.Equal(t, uint64(1000), calls[0].Limit)
 }
+
+func TestRunner__Processor_Timer_Expired(t *testing.T) {
+	repo := &RepositoryMock{}
+	r := New(repo, WithRetryDuration(20*time.Millisecond))
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	repo.GetLastEventsFunc = func(ctx context.Context, limit uint64) ([]Event, error) {
+		return nil, nil
+	}
+	repo.GetUnprocessedEventsFunc = func(ctx context.Context, limit uint64) ([]Event, error) {
+		return nil, nil
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		r.Run(ctx)
+	}()
+
+	time.Sleep(40 * time.Millisecond)
+	cancel()
+	wg.Wait()
+
+	assert.Equal(t, 1, len(repo.GetLastEventsCalls()))
+	assert.Equal(t, 1, len(repo.GetUnprocessedEventsCalls()))
+}
+
+func TestRunner_Processor_Run_Error(t *testing.T) {
+	repo := &RepositoryMock{}
+	publisher := &PublisherMock{}
+
+	r := New(repo, WithRetryDuration(20*time.Millisecond),
+		WithPublisher(100, publisher),
+	)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	repo.GetLastEventsFunc = func(ctx context.Context, limit uint64) ([]Event, error) {
+		return nil, nil
+	}
+	repo.GetLastSequenceFunc = func(ctx context.Context, id PublisherID) (uint64, error) {
+		return 0, nil
+	}
+	repo.GetUnprocessedEventsFunc = func(ctx context.Context, limit uint64) ([]Event, error) {
+		return []Event{
+			{ID: 100, Sequence: 10},
+			{ID: 99, Sequence: 18},
+		}, nil
+	}
+	repo.UpdateSequencesFunc = func(ctx context.Context, events []Event) error {
+		return errors.New("update-seq-error")
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		r.Run(ctx)
+	}()
+
+	time.Sleep(40 * time.Millisecond)
+	cancel()
+	wg.Wait()
+
+	assert.Equal(t, 2, len(repo.GetLastEventsCalls()))
+}
